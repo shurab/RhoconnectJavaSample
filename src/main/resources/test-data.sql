15a3e15f69c6d5d1e4e72205d076a473664f0c0d
Alexander Babichev <alexb@rhomobile.com>
2011-11-03 18:08:33 -0700

    Replaced MySQL db by embedded in-memory HSQL.

---
+++ src/main/resources/test-data.sql
@@ -0,0 +1,6 @@
+insert into contacts (id, firstname, lastname, telephone, email, created) 
+values (null, 'Bruce', 'Phillips', '510-456-2012', 'bruce@gmail.com', '2011-11-03 16:50:01');
+
+insert into contacts (id, firstname, lastname, telephone, email, created) 
+values (null, 'Viral', 'Patel', '408-123-4567', 'patel@yahoo.com', '2011-11-03 16:51:30');
+
